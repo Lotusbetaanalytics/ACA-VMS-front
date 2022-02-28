5a48572647eb8ae6a2dd6390b6d9954a409cd784
ejoor emmanuel <ejooremmanuel@gmail.com>
2022-02-28 18:11:42 +0100

    socket url

--- src/components/LogsTable.js
+++ src/components/LogsTable.js
@@ -33,7 +33,6 @@ import axios from "axios";
 import Moment from "react-moment";
 import "moment-timezone";
 import _ from "lodash";
-import { BASE_URL } from "../redux/constants/constants";
 
 const tableIcons = {
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
@@ -89,7 +88,7 @@ function LogTable({ data, title = "Visitors Logs" }) {
   const checkedInHandler = (id) => {
     setIsLoading(true);
     axios
-      .patch(`${BASE_URL}/guest/checkin/${id}`)
+      .patch(`/api/v1/guest/checkin/${id}`)
       .then((res) => {
         setIsLoading(false);
         toast({
@@ -122,7 +121,7 @@ function LogTable({ data, title = "Visitors Logs" }) {
   const checkedOutHandler = (id) => {
     setIsLoading(true);
     axios
-      .patch(`${BASE_URL}/guest/checkout/${id}`)
+      .patch(`/api/v1/guest/checkout/${id}`)
       .then((res) => {
         setIsLoading(false);
         toast({
